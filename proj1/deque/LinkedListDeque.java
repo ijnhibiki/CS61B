d986769987c35ad7a4ea9249c57d4e5f51e62ad8
Haobo Chen <chenhaob@grinnell.edu>
2022-09-06 11:07:08 -0500

    9.5

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,82 @@
+package deque;
+
+
+public class LinkedListDeque<T> {
+    private class Deque {
+        public Deque next;
+        public Deque prev;
+        public T item;
+
+        public Deque(Deque p, T i, Deque n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private Deque sentinel;
+
+    private int size;
+
+
+
+    public LinkedListDeque() {
+        sentinel = new Deque(null, null, null);
+        sentinel.next = sentinel.prev;
+        size = 0;
+    }
+
+    public LinkedListDeque(T item) {
+        sentinel = new Deque(null,null, null);
+        sentinel.next = new Deque(null,item, null);
+        size = 1;
+    }
+
+
+
+
+    public void addFirst(T item) {
+        sentinel.next = new Deque(null, item, sentinel.next);
+        size = size +1;
+    }
+
+    public void addLast(T item){
+        return;
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+
+    public T getFirst() {
+        return sentinel.next.item;
+    }
+
+    public void printDeque(){
+        return;
+    }
+
+    public T removeFirst(){
+        return null;
+    }
+
+    public T removeLast(){
+        return null;
+    }
+
+    public T get(int index){
+        return null;
+    }
+    public int size() {
+        return size;
+    }
+
+
+    public static void main(String[] args) {
+        /* Creates a list of one integer, namely 10 */
+        LinkedListDeque <Integer> L = new LinkedListDeque<Integer>();
+        L.addFirst(20);
+        System.out.println(L.size());
+    }
+}
