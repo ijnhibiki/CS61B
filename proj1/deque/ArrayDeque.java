ff731a0dd283cbfb3fbceec8d7eb98c39544a0b3
Haobo Chen <chenhaob@grinnell.edu>
2022-09-09 21:31:03 -0500

    9.9

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,5 +1,98 @@
 package deque;
 
 public class ArrayDeque<T> {
+    private int size;
+    private T[] items;
+    public int nextFirst;
+    public int nextLast;
 
+
+    public ArrayDeque() {
+        items = (T [] ) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    private void resize(int capacity, int indicator) {
+        if (indicator == 1) {
+            int first = nextFirst;
+            int last = nextLast;
+            T[] TempArray = (T[]) new Object[capacity/2];
+            System.arraycopy(items, first + 1, TempArray, 0, size - first - 1);
+            System.arraycopy(items, 0, TempArray, size - first - 1, last);
+            T[] NewArray = (T[]) new Object[capacity];
+            System.arraycopy(TempArray, 0, NewArray, 5, size);
+            items = NewArray;
+            nextFirst = 4;
+            nextLast = 4 + size;
+        }
+        if (indicator == 2) {
+
+        }
+    }
+
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 2, 1);
+        }
+        if (nextFirst == -1 && size < items.length) {
+            nextFirst = items.length - 1;
+        }
+        items[nextFirst] = item;
+        size = size + 1;
+        nextFirst = nextFirst - 1;
+    }
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * 2, 1);
+        }
+        if (nextLast == items.length && size < items.length) {
+            nextLast = 0;
+        }
+        items[nextLast] = item;
+        size = size + 1;
+        nextLast = nextLast + 1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int index = nextFirst + 1;
+        for (int counter = 0; counter < size; counter ++) {
+            if (index == items.length) {
+                index = 0;
+            }
+            System.out.print(get(index) + " ");
+            index += 1;
+        }
+    }
+
+    public T removeFirst() {
+        T FirstValue = get(0);
+        items[nextFirst + 1] = null;
+        size = size - 1;
+        return FirstValue;
+    }
+
+    public T removeLast() {
+        T LastValue = get(0);
+        items[nextLast - 1] = null;
+        size = size - 1;
+        return LastValue;
+    }
+
+    public T get(int index) {
+        return items[index];
+    }
 }
---
+++ proj1/deque/ArrayDequeTest.java
@@ -0,0 +1,84 @@
+package deque;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArrayDequeTest {
+
+    @Test
+    public void addIsEmptySizeTest() {
+        ArrayDeque<String> lld1 = new ArrayDeque<>();
+
+        assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
+        lld1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        assertEquals(1, lld1.size());
+        assertFalse("lld1 should now contain 1 item", lld1.isEmpty());
+
+        lld1.addLast("middle");
+        assertEquals(2, lld1.size());
+
+        lld1.addLast("back");
+        assertEquals(3, lld1.size());
+
+        System.out.println("Printing out deque: ");
+        lld1.printDeque();
+    }
+
+    @Test
+    public void ArraycircularTest() {
+        ArrayDeque<Integer> lld1 = new ArrayDeque<>();
+
+        assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
+
+       for (int i = 0; i < 7; i ++) {
+           lld1.addFirst(i);
+       }
+       assertEquals(lld1.size(), 7);
+       System.out.println("Printing out deque: ");
+       lld1.printDeque();
+
+       ArrayDeque<Integer> lld2 = new ArrayDeque<>();
+
+       assertTrue("A newly initialized LLDeque should be empty", lld2.isEmpty());
+       for (int i = 0; i < 7; i ++) {
+            lld2.addLast(i);
+        }
+       assertEquals(lld2.size(), 7);
+    }
+
+    @Test
+    public void addRemoveTest() {
+
+
+        ArrayDeque<Integer> lld1 = new ArrayDeque<>();
+        // should be empty
+        assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
+
+        lld1.addFirst(10);
+        // should not be empty
+        assertFalse("lld1 should contain 1 item", lld1.isEmpty());
+        lld1.addLast(20);
+        lld1.addLast(30);
+
+        lld1.removeFirst();
+        // should be empty
+        assertFalse("lld1 should be empty after removal", lld1.isEmpty());
+
+        assertEquals(lld1.get(5), (Object) 20);
+    }
+    @Test
+    public void Resize1Test() {
+
+
+        ArrayDeque<Integer> lld1 = new ArrayDeque<>();
+        // should be empty
+        assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
+        for (int i = 0; i < 10; i++) {
+            lld1.addFirst(i);
+        }
+        assertEquals(lld1.size(), 10);
+    }
+}
