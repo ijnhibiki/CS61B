99278dd2eb9b105655ff05ab52c06da0e756660f
Haobo Chen <chenhaob@grinnell.edu>
2022-09-12 10:42:26 -0500

    restored Collatz

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -3,15 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        // TODO: Fill in this method.
+        if (n % 2 == 0) {
+            return n / 2;
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
+            return 1;
+
     }
 
     public static void main(String[] args) {
