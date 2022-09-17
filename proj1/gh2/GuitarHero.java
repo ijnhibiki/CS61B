7a7bc78aeb1179edfec2b45ffd0f227a3bba0a78
Haobo Chen <chenhaob@grinnell.edu>
2022-09-17 13:36:17 -0500

    Guitar

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,42 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+import deque.ArrayDeque;
+
+public class GuitarHero {
+    public static void main(String[] args) {
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        ArrayDeque<GuitarString> keyArray = new ArrayDeque<>();
+        double sound;
+        for (int i = 0; i < keyboard.length(); i += 1) {
+            sound = 440.0 * Math.pow(2, (i - 24) / 12.0);
+            GuitarString stringSound = new GuitarString(sound);
+            keyArray.addLast(stringSound);
+        }
+
+    while (true) {
+
+        /* check if the user has typed a key; if so, process it */
+        if (StdDraw.hasNextKeyTyped()) {
+            char key = StdDraw.nextKeyTyped();
+            int index = keyboard.indexOf(key);
+            if (index >= 0 && index < 37) {
+                keyArray.get(index).pluck();
+            }
+        }
+
+
+        double sample = 0.0;
+        for (int i = 0; i < keyboard.length(); i++) {
+            sample += keyArray.get(i).sample();
+        }
+
+
+        StdAudio.play(sample);
+        for (int i = 0; i < keyboard.length(); i++) {
+            keyArray.get(i).tic();
+        }
+    }
+
+    }
+}
--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -2,7 +2,8 @@ package gh2;
 
 
 import deque.Deque;
-// TODO: maybe more imports
+import deque.LinkedListDeque;
+
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -13,23 +14,25 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+    private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
+        buffer = new LinkedListDeque<>();
+        for (int i = 0; i < ((int) Math.round(SR / frequency)); i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
+        for (int i = 0; i < buffer.size(); i++) {
+            double r = Math.random() - 0.5;
+            buffer.removeFirst();
+            buffer.addLast(r);
+        }
+
         //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
@@ -41,15 +44,13 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        double second = buffer.get(1);
+        double front = buffer.removeFirst();
+        buffer.addLast(((front + second) / 2) * DECAY);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
