0da181c2b52a2988c93ad792f538d385eacfb77c
Haobo Chen <chenhaob@grinnell.edu>
2022-09-17 12:32:50 -0500

    Update MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,18 +1,19 @@
 package deque;
 import java.util.Comparator;
 
-public class MaxArrayDeque<T> extends ArrayDeque <T> {
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
     private Comparator<T>  comp;
     public MaxArrayDeque(Comparator<T> c) {
         super();
         comp = c;
     }
-    public T max(){
+
+    public T max() {
         if (isEmpty()) {
             return null;
         }
         int maxDex = 0;
-        for (int i = 0; i < size(); i ++) {
+        for (int i = 0; i < size(); i++) {
             int indicator = comp.compare(get(i), get(maxDex));
             if (indicator > 0) {
                 maxDex = i;
@@ -20,19 +21,19 @@ public class MaxArrayDeque<T> extends ArrayDeque <T> {
         }
         return get(maxDex);
     }
+
     public T max(Comparator<T> c) {
         if (isEmpty()) {
             return null;
         }
         comp = c;
         int maxDex = 0;
-        for (int i = 0; i < size(); i ++) {
+        for (int i = 0; i < size(); i++) {
             int indicator = comp.compare(get(i), get(maxDex));
             if (indicator > 0) {
                 maxDex = i;
             }
         }
-
         return get(maxDex);
     }
 }
