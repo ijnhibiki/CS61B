4c1c5d1a4afdc04dec0ae609400e2b3b46041faf
Haobo Chen <chenhaob@grinnell.edu>
2022-09-17 01:24:00 -0500

    Update MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,6 +1,27 @@
 package deque;
 
 
-public class MaxArrayDeque {
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque <T> {
+    ArrayDeque<T> MaxArrayDeque;
+    public MaxArrayDeque(Comparator<T> c) {
+        MaxArrayDeque = new ArrayDeque<>();
+        Comparator<T> current = c;
+
+    }
+    public T max() {
+        if (MaxArrayDeque.isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+    public T max(Comparator<T> c) {
+        if (MaxArrayDeque.isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
 
 }
